e48633cbf87df06b7ec585d1ab66853b783eedd8
MeryAmr <amrelkammah77@gmail.com>
2025-04-19 21:25:13 +0200

    Fixed order service and tested it

--- order-service/internal/models/order-item.go
+++ order-service/internal/models/order-item.go
@@ -1,8 +1,8 @@
 package models
 
 type OrderItem struct {
-	ItemID   string  `json:"item_id"`
-	Name     string  `json:"name"`
-	Quantity int     `json:"quantity"`
-	Price    float64 `json:"price"`
+	ItemID   string  `json:"item_id" bson:"item_id"`
+	Name     string  `json:"name" bson:"name"`
+	Quantity int     `json:"quantity" bson:"quantity"`
+	Price    float64 `json:"price" bson:"price"`
 }
--- order-service/internal/models/order.go
+++ order-service/internal/models/order.go
@@ -5,11 +5,11 @@ import (
 )
 
 type Order struct {
-	ID        string      `json:"id"`
-	UserID    string      `json:"user_id"`
-	Items     []OrderItem `json:"items"`
-	Total     float64     `json:"total"`
-	Status    string      `json:"status"`
-	CreatedAt time.Time   `json:"created_at"`
-	UpdatedAt time.Time   `json:"updated_at"`
+	ID        string      `json:"id" bson:"id"`
+	UserID    string      `json:"user_id" bson:"user_id"`
+	Items     []OrderItem `json:"items" bson:"items"`
+	Total     float64     `json:"total" bson:"total"`
+	Status    string      `json:"status" bson:"status"`
+	CreatedAt time.Time   `json:"created_at" bson:"created_at"`
+	UpdatedAt time.Time   `json:"updated_at" bson:"updated_at"`
 }
